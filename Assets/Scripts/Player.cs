7a91d296ea5c1e28823f831e4575e78902a11087
Sergey Blednov <sergey.blednov@pixacore.com>
2017-05-15 19:01:52 -0400

    Added zombie, navmesh, zoom.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Transform playerSpawnPoints;
+	public bool reSpawn = false;
+
+	private Transform[] spawnPoints;
+	private bool lastToggle = false;
+
+	// Use this for initialization
+	void Start () {
+		spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform> ();
+	}
+
+	void Update () {
+		if (lastToggle != reSpawn) {
+			ReSpawn ();
+		} else {
+			lastToggle = reSpawn;
+		}
+	}
+
+	void ReSpawn () {
+		reSpawn = false;
+		transform.position = spawnPoints[Random.Range (0, spawnPoints.Length)].position;
+
+	}
+}
