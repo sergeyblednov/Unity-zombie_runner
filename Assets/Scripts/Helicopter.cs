ab1c29711ce8d8e39907558be6f00d55a8ed4e7f
Sergey Blednov <sergey.blednov@pixacore.com>
2017-05-16 18:26:06 -0400

    Added zoom and a helicopter

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+	public AudioClip callSound;
+	private bool called = false;
+
+	void Update () {
+		if (Input.GetButtonDown("CallHeli") && !called) {
+			called = true;
+			print ("Helicopter called");
+			AudioSource source = GetComponent<AudioSource> ();
+			source.clip = callSound;
+			source.Play ();
+		}
+	}
+}
